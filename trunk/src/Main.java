e1b025bab64c33876f4f75da5ac394383cb9766a
amaurymedeiros@gmail.com <amaurymedeiros@gmail.com@46f8988c-cdbc-99d8-8c54-79eeae60919d>
2011-06-06 03:12:49 +0000

    Classes modularizadas.

--- trunk/src/Main.java
+++ trunk/src/Main.java
@@ -21,80 +21,23 @@ import org.apache.lucene.util.Version;
 
 public class Main {
 
-	private static IndexWriter indexWriter;
-	private static IndexReader indexReader;
-	private static IndexSearcher indexSearcher;
-	private static String path = "colecao";
-	private static Analyzer analyzer;
-	private static File root;
-
-	private static void addDoc(IndexWriter w, String value) throws IOException {
-		Document doc = new Document();
-		doc.add(new Field("title", value, Field.Store.YES, Field.Index.ANALYZED));
-		w.addDocument(doc);
-	}
-
-	private static void indexFilesFromRoot(IndexWriter writer, File file)
-			throws IOException {
-		if (file.canRead()) {
-			if (file.isDirectory()) {
-				String[] files = file.list();
-				// an IO error could occur
-				if (files != null) {
-					for (int i = 0; i < files.length; i++) {
-						indexFilesFromRoot(writer, new File(file, files[i]));
-					}
-				}
-			} else {
-				System.out.println("adding " + file);
-				FileReader fr = null;
-				try {
-					fr = new FileReader(file);
-					Document doc = new Document();
-					doc.add(new Field("contents", fr));
-					doc.add(new Field("path", file.getAbsolutePath(),
-							Field.Store.YES, Field.Index.ANALYZED));
-					writer.addDocument(doc);
-				}
-				// at least on windows, some temporary files raise this
-				// exception with an "access denied" message
-				// checking if the file can be read doesn't help
-				catch (IOException e) {
-					System.out.println("Ignorando o arquivo " + file);
-				} finally {
-					fr.close();
-				}
-			}
-		}
-
-	}
-
 	public static void main(String[] args) {
-		analyzer = new BrazilianAnalyzer(Version.LUCENE_32,
+		String path = "colecao";
+		Analyzer analyzer = new BrazilianAnalyzer(Version.LUCENE_32,
 				BrazilianAnalyzer.getDefaultStopSet());
-		root = new File(path);
+		File root = new File(path);
 		try {
 			Directory d = new SimpleFSDirectory(root);
-			indexWriter = new IndexWriter(d, new IndexWriterConfig(
-					Version.LUCENE_32, analyzer));
-			indexFilesFromRoot(indexWriter, root);
-			indexWriter.optimize();
-			indexWriter.close();
-			indexSearcher = new IndexSearcher(d, true);
-			String query = "ARA";
+			Indexer indexer = new Indexer(root, d, analyzer);
+			indexer.indexAll();
+
+			String query = "roofing";
 			int hitsPerPage = 10;
-			Query q = new QueryParser(Version.LUCENE_32, "contents", analyzer)
-					.parse(query);
-			TopScoreDocCollector collector = TopScoreDocCollector.create(
-					hitsPerPage, true);
-			indexSearcher.search(q, collector);
-			ScoreDoc[] hits = collector.topDocs().scoreDocs;
-			System.out.println("Found " + hits.length + " hits.");
-			for (int i = 0; i < hits.length; ++i) {
-				int docId = hits[i].doc;
-				Document doc = indexSearcher.doc(docId);
-				System.out.println((i + 1) + ". " + doc.get("path"));
-			}
+
+			
+			Searcher searcher = new Searcher(d, query, "contents", analyzer, hitsPerPage);
+			searcher.search();
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (ParseException e) {
