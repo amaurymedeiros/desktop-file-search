990f17a100a5fa08ca1bcb33662557359dc13262
dsurviver@gmail.com <dsurviver@gmail.com@46f8988c-cdbc-99d8-8c54-79eeae60919d>
2011-06-07 23:40:29 +0000

    Mudando nomes de arquivos .py para consulta curinga Removendo parametros nao utilizados no contrutor de Searcher Evitando a indexacao de pastas comecando com '.' Criacao do campo NAME_FIELD Utilizando MultiFieldQueryParser para realizar a consulta nos campos NAME_FIELD e CONTENT_FIELD

--- trunk/src/Main.java
+++ trunk/src/Main.java
@@ -14,82 +14,82 @@ import org.apache.lucene.store.SimpleFSDirectory;
 import org.apache.lucene.util.Version;
 
 public class Main {
-    // No combobox da interface nao colocar so a extensao. Colocar mais info.
+	// No combobox da interface nao colocar so a extensao. Colocar mais info.
 	// Ex: Arquivos de texto (.txt), Arquivos python (.py), etc.
-	private static String[] formats = {"txt", "pdf", "doc", "py", "c", "java"};
+	private static String[] formats = { "txt", "pdf", "doc", "docx", "py", "c", "cpp",
+			"java" };
 
-	
-	public static void main(String[] args) throws java.text.ParseException, ParseException {
+	public static void main(String[] args) throws java.text.ParseException,
+			ParseException {
 		String path = "colecao";
 		Analyzer analyzer = new BrazilianAnalyzer(Version.LUCENE_32,
 				BrazilianAnalyzer.getDefaultStopSet());
 		File root = new File(path);
 		try {
 			Directory indexDir = new SimpleFSDirectory(new File("indices"));
-			
-			//Pergunta se deseja reindexar.
+
+			// Pergunta se deseja reindexar.
 			System.out
 					.println("Voce gostaria de realizar uma indexação na sua coleção? (S para sim, qualquer outra coisa para não)");
 			Scanner sc = new Scanner(System.in);
 			String resp = sc.nextLine();
-			
+
 			if (resp.trim().toLowerCase().equals("s")) {
-				Indexer indexer = new Indexer(root, indexDir, analyzer,formats);
+				Indexer indexer = new Indexer(root, indexDir, analyzer, formats);
 				indexer.indexAll();
-			} 
-				
+			}
 
 			String query = "";
 			System.out.println("Digite a consulta que quer realizar:");
 			query = sc.nextLine().trim();
 			query = query.equals("") ? null : query;
+			String formats[] = null;
 			int hitsPerPage = 10;
-            try {
-            	
-            	System.out.println("Digite o tipo de arquivo que quer pesquisar. (Tipos invalidos serao desconsiderados).");
-            	String tipo = sc.nextLine().trim();
-            	tipo = tipo.equals("") ? null : tipo;
-            	
-            	String dateFormat = "dd/MM/yyyy - hh:mm";
-            	SimpleDateFormat formatador = new SimpleDateFormat(dateFormat);
+			try {
 
-            	
-            	System.out.println("Formato das datas: " + dateFormat);
-            	System.out.println("Informe a data de inicio. Datas invalidas serao desconsideradas.");
-            	
-            	Date min;
+				System.out
+						.println("Digite o tipo de arquivo que quer pesquisar. (Tipos invalidos serao desconsiderados).");
+				String tipo = sc.nextLine().trim();
+				formats = tipo.equals("") ? null : tipo.split(";");
+
+				String dateFormat = "dd/MM/yyyy - hh:mm";
+				SimpleDateFormat formatador = new SimpleDateFormat(dateFormat);
+
+				System.out.println("Formato das datas: " + dateFormat);
+				System.out
+						.println("Informe a data de inicio. Datas invalidas serao desconsideradas.");
+
+				Date min;
 				try {
 					min = formatador.parse(sc.nextLine().trim());
 				} catch (java.text.ParseException e) {
 					min = null;
 				}
-				System.out.println("Informe a data final. Datas invalidas serao desconsideradas.");
-            	Date max;
+				System.out
+						.println("Informe a data final. Datas invalidas serao desconsideradas.");
+				Date max;
 				try {
 					max = formatador.parse(sc.nextLine().trim());
 				} catch (java.text.ParseException e) {
 					max = null;
 				}
-				
+
 				System.out.println("Quantos resultados gostaria de exibir?");
 				try {
 					hitsPerPage = Integer.parseInt(sc.nextLine().trim());
-				} catch (Exception e){
+				} catch (Exception e) {
 					//
 				}
-				Searcher searcher = new Searcher(indexDir, query, "contents",
-    					analyzer, hitsPerPage);
-				
-				String[] format = {"doc"};
-				
-    			searcher.search(null,query,format, min, max,0,Long.MAX_VALUE);
-				//searcher.search();
+				Searcher searcher = new Searcher(indexDir, analyzer, hitsPerPage);
+
+				searcher.search(query, formats, min, max, 0,
+						Long.MAX_VALUE);
+				// searcher.search();
 			} catch (IndexNotFoundException e) {
 				System.out.println("Sua coleção ainda não foi indexada.");
 			} catch (NoSuchDirectoryException e) {
 				System.out.println("Sua coleção ainda não foi indexada.");
 			}
-			
 
 		} catch (IOException e) {
 			e.printStackTrace();
--- trunk/src/Searcher.java
+++ trunk/src/Searcher.java
@@ -6,7 +6,7 @@ import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.document.DateTools;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.CorruptIndexException;
-import org.apache.lucene.index.Term;
+import org.apache.lucene.queryParser.MultiFieldQueryParser;
 import org.apache.lucene.queryParser.ParseException;
 import org.apache.lucene.queryParser.QueryParser;
 import org.apache.lucene.search.BooleanClause;
@@ -14,7 +14,6 @@ import org.apache.lucene.search.BooleanQuery;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
-import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.search.TopScoreDocCollector;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.util.Version;
@@ -27,34 +26,34 @@ public class Searcher {
 
 	private Analyzer analyzer;
 
-	public Searcher(Directory d, String query, String field, Analyzer analyzer,
-			int hitsPerPage) throws CorruptIndexException, IOException {
+	public Searcher(Directory d, Analyzer analyzer, int hitsPerPage)
+			throws CorruptIndexException, IOException {
 		this.indexSearcher = new IndexSearcher(d, true);
 		this.hitsPerPage = hitsPerPage;
 		this.analyzer = analyzer;
 	}
-	
 
-
-	public void search(String path,String query, String[] formato, Date min, Date max,
-			long sizeMin, long sizeMax) throws java.text.ParseException, CorruptIndexException, IOException, ParseException {
+	public void search(String query, String[] formato, Date min, Date max,
+			long sizeMin, long sizeMax) throws java.text.ParseException,
+			CorruptIndexException, IOException, ParseException {
 		BooleanQuery q = new BooleanQuery();
-			
-		if(path != null){
-			Query q12 = new QueryParser(Version.LUCENE_32, Indexer.PATH_FIELD, analyzer).parse(path);
-			q.add(q12,BooleanClause.Occur.MUST);
-		}
+
 		if (query != null) {
-			Query q13 = new QueryParser(Version.LUCENE_32, Indexer.CONTENT_FIELD, analyzer).parse(query);
-			q.add(q13,BooleanClause.Occur.MUST);
+			MultiFieldQueryParser queryParser = new MultiFieldQueryParser(
+					Version.LUCENE_32, Indexer.FIELDS, analyzer);
+			queryParser.setAllowLeadingWildcard(true);
+			q.add(queryParser.parse(query), BooleanClause.Occur.MUST);
 		}
 		if (formato != null && formato.length > 0) {
 			BooleanQuery aux = new BooleanQuery();
-			for(String n : formato){
-				Query q14 = new QueryParser(Version.LUCENE_32, Indexer.FORMATO_FIELD, analyzer).parse(n);
-				aux.add(q14,BooleanClause.Occur.SHOULD);
+			for (String n : formato) {
+				QueryParser parser = new QueryParser(Version.LUCENE_32,
+						Indexer.FORMATO_FIELD, analyzer);
+				parser.setAllowLeadingWildcard(true);
+				Query q14 = parser.parse(n);
+				aux.add(q14, BooleanClause.Occur.SHOULD);
 			}
-			q.add(aux,BooleanClause.Occur.MUST);
+			q.add(aux, BooleanClause.Occur.MUST);
 		}
 		Date m1 = min;
 		Date m2 = max;
@@ -64,12 +63,12 @@ public class Searcher {
 		if (max == null) {
 			m2 = new Date(Calendar.getInstance().getTimeInMillis());
 		}
-		
+
 		TopScoreDocCollector collector = TopScoreDocCollector.create(
 				hitsPerPage, true);
 		this.indexSearcher.search(q, collector);
 		ScoreDoc[] hits = collector.topDocs().scoreDocs;
-		
+
 		int quant_results = 0;
 		System.out.println("Total: " + hits.length + " resultados.");
 		for (int i = 0; i < hits.length; ++i) {
@@ -83,16 +82,17 @@ public class Searcher {
 			Date d = DateTools.stringToDate(doc_data);
 			if (d.before(m2) && d.after(m1)) {
 				date_ok = true;
-			}if(sizeMin < size && size < sizeMax){
+			}
+			if (sizeMin < size && size < sizeMax) {
 				size_ok = true;
 			}
-			if(date_ok && size_ok){
+			if (date_ok && size_ok) {
 				System.out.println((++quant_results) + ". " + doc.get("path"));
 				System.out.println("Sumário:");
-				System.out.println(this.indexSearcher.explain(q, docId).toString());
+				System.out.println(this.indexSearcher.explain(q, docId)
+						.toString());
 			}
 		}
-		
-	}
 
+	}
 }
